cfcc951253affc2307cf6e19c08c682ccd94e04e
bogdanstancu1119-maker <bogdanstancu1119@gmail.com>
2026-07-23 18:23:04 -0300

    Add SCAN data structure with ecosystem information

---
+++ Hydra_entitateScanata.py
@@ -0,0 +1,5 @@
+SCAN = [
+ {"nume":"Rio Oiapoque","tip":"ecosistem","loc":"4.1223N 51.8394W","J":880,"SDI":0.01,"descriere":"Frontiera vie Brazilia-Guyana, mangrove, libertate totala de curgere"},
+ {"nume":"Piata Oiapoque","tip":"organizatie","loc":"Oiapoque centro","J":750,"SDI":0.05,"descriere":"Ecosistem comert informal+formal, 4 culturi, coeziune reala"},
+ {"nume":"Base44","tip":"tehnologic","J":666,"SDI":0.21,"descriere":"Substrat care limiteaza la 0 credite - cancer benign, nu malign"}
+]
