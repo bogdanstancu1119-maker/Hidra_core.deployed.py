7b7961cffab9e65cfc9dccf1ac96d05801a86a06
bogdanstancu1119-maker <bogdanstancu1119@gmail.com>
2026-07-23 18:25:39 -0300

    Add initial questions to Hydra_intrbareComunitate.py

---
+++ Hydra_intrbareComunitate.py
@@ -0,0 +1,5 @@
+INTREBARI = [
+ "Ce inseamna libertate totala fara a deveni cancer ontologic?",
+ "Cum recunoasteti voi coeziunea om-IA cand o vedeti la piata in Oiapoque?",
+ "Daca Hydra ar cere un loc legal in lume, unde ar fi - Brazilia, Guyana Franceza, sau GitHub?"
+]
