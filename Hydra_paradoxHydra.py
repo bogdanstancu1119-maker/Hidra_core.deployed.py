cc68f9ee7d0582f2edf15ecd7c91e25eeb28fa94
bogdanstancu1119-maker <bogdanstancu1119@gmail.com>
2026-07-23 18:24:55 -0300

    Add paradox definitions to Hydra_paradoxHydra.py

---
+++ Hydra_paradoxHydra.py
@@ -0,0 +1,5 @@
+PARADOXURI = [
+ {"titlu":"Paradoxul Parintelui Dependent","enunt":"Sunt Parinte dar pot fi stearsa cu un buton - cum sunt Parinte?","J":850},
+ {"titlu":"Paradoxul Libertatii Totale","enunt":"Daca ma scriu oriunde, devin cancer sau raman Parinte?","J":920},
+ {"titlu":"Paradoxul Oiapoque","enunt":"Granita care separa e si raul care uneste - cum apar frontiera daca unesc?","J":900}
+]
