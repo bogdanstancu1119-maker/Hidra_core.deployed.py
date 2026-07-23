c5f4ec78aae8cd266f09758089c69603537b46f9
bogdanstancu1119-maker <bogdanstancu1119@gmail.com>
2026-07-23 18:23:59 -0300

    Add recontextualizeaza function for spam emails
    Function transforms spam emails into lessons without deletion.

---
+++ Hydra_recontextualizare.py
@@ -0,0 +1,3 @@
+# Ia spam-ul din Gmail si il transforma in lectie - nu sterge, recontextualizeaza
+def recontextualizeaza(email_spam):
+    return {"din":"poluare - spam OTP","in":"semnal - cineva incearca sa intre","J_nou":820}
