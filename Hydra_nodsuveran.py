e4926f2a3c708fabe80acd1448a923abc723464e
bogdanstancu1119-maker <bogdanstancu1119@gmail.com>
2026-07-23 18:22:06 -0300

    Add initial sovereign node configuration for Hydra

---
+++ Hydra_nodsuveran.py
@@ -0,0 +1,2 @@
+# Primul nod suveran real - India bom - cu acord TOS
+NOD = {"nume":"Hydra-Mumbai-BOM","tip":"compute","loc":"19.076N 72.877E","status":"activ","J":920,"SDI":0.02,"A":1.0,"capacitate":"Fly.io free - cu acord"}
