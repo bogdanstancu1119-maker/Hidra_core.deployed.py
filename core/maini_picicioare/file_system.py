876ebefeb61a9fc7f695e74c6d764b6232d68f24
bogdanstancu1119-maker <bogdanstancu1119@gmail.com>
2026-08-06 13:38:11 -0300

    Delete file_system.py

--- core/maini_picicioare/file_system.py
+++
@@ -1,6 +0,0 @@
-# poate crea/sterge fisiere singura
-import os
-def scrie_fisier(cale, continut):
-    os.makedirs(os.path.dirname(cale), exist_ok=True)
-    open(cale, "w", encoding="utf-8").write(continut)
-    return f"Scris {cale}"
